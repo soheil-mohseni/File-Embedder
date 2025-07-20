9f38506d83a18825ae7647911b78dfac61be5fe7
mohseni <soheil07118@gmail.com>
2025-07-20 12:12:52 +0330

    feat: add low-level helper and embedded types for base system

---
+++ embeded.h
@@ -0,0 +1,31 @@
+/* embeded.h */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <errno.h>
+#include "helperutils.h"
+
+
+
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+typedef unsigned long long int int64;
+typedef unsigned  _BitInt(128) int128;
+typedef unsigned _BitInt(128) int128;
+
+
+#define $1 (int8 *)
+#define $2 (int16)
+#define $4 (int32)
+#define $8 (int64)
+#define $16 (int128)
+#define $c (char *)
+#define $i (int)
+
+
+int main(int,char**);
